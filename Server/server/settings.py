2dac696d25d7b49a06e07b888218e6e67057ce2b
melnyczuk <h.melnyczuk@gmail.com>
2017-09-02 18:54:15 +0100

    sshhhhh

--- Server/server/settings.py
+++ Server/server/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = './secretsettings.txt'
-GOOGLE_OAUTH2_CLIENT_SECRETS_JSON = "./server/client_secret_2.json"
-#GOOGLE_OAUTH_CLIENT_SECRET = "KwwSIjYjFagjPac9-9buPSEJ"
-#GOOGLE_OAUTH_CLIENT_ID = "240841548439-806a8ge6r5d1li7tqe4g8f0iocg5ihnf.apps.googleusercontent.com"
+#GOOGLE_OAUTH2_CLIENT_SECRETS_JSON = "./client_secret_2.json"
+GOOGLE_OAUTH_CLIENT_SECRET = "KwwSIjYjFagjPac9-9buPSEJ"
+GOOGLE_OAUTH_CLIENT_ID = "240841548439-806a8ge6r5d1li7tqe4g8f0iocg5ihnf.apps.googleusercontent.com"
 GOOGLE_OAUTH_SCOPE = ["https://www.googleapis.com/auth/gmail.readonly",]
 GOOGLE_OAUTH_REDIRECT_URI = "http://127.0.0.1:8000/backend/callback"
 
